caf8e025630d9839b3576e13ac0bcfc87e7e1a39
Vladimir <vladimir.cherepanov@cern.ch>
2019-02-21 14:46:35 +0100

    commit small change before merging

--- T3MNtuple/test/analyze.py
+++ T3MNtuple/test/analyze.py
@@ -12,7 +12,7 @@ options.register('globalTag',
                  "Global Tag")
 
 options.register('nEvents',
-                 -1, #default value
+                 300, #default value
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.int,
                  "Maximum number of processed events")
