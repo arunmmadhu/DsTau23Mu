b8dc658f4dab3a91de25434fa4730f903fb08393
Vladimir Cherepanov <vladimir.cherepanov@cern.ch>
2023-07-17 13:42:56 +0200

    complained in 3 vector var for some reason

--- T3MNtuple/src/classes.h
+++ T3MNtuple/src/classes.h
@@ -3,6 +3,7 @@
 #include <DataFormats/PatCandidates/interface/PFParticle.h>
 #include <DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h>
 
+std::vector< std::vector<std::vector<float> > >    dummy4;
 std::vector< std::vector<std::vector<double> > >    dummy5;
 std::vector< std::vector<int > >    dummy6;
 std::vector< std::vector<unsigned int> >     dummy7;
